9e92df07b3faa5158a5368a0281b2984210cc451
Koji Eguchi <koji.eguchi@hotmail.com>
2024-02-04 20:11:01 -0500

    Add Resume model

---
+++ db/models/resumes.js
@@ -0,0 +1,153 @@
+const mongoose = require("../db");
+const { v4: uuidv4 } = require("uuid");
+
+const resumeSchema = new mongoose.Schema({
+  resume_id: {
+    type: String,
+    default: uuidv4,
+    required: true,
+    unique: true,
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  summary: String,
+  skills: [String],
+  education: [
+    {
+      degree: String,
+      school: String,
+      major: String,
+      graduationYear: Number,
+    },
+  ],
+  workExperience: [
+    {
+      jobTitle: String,
+      company: String,
+      location: String,
+      startDate: Date,
+      endDate: Date,
+      responsibilities: [String],
+      achievements: [String],
+    },
+  ],
+  projects: [
+    {
+      projectName: String,
+      description: String,
+      rolesResponsibilities: [String],
+      technologiesUsed: [String],
+    },
+  ],
+  certifications: [
+    {
+      certificationName: String,
+      issuingOrganization: String,
+      dateEarned: Date,
+    },
+  ],
+  languages: [
+    {
+      languageName: String,
+      proficiencyLevel: String,
+    },
+  ],
+  references: [
+    {
+      referenceName: String,
+      relationship: String,
+      contactInformation: String,
+    },
+  ],
+  contactInformation: {
+    phoneNumber: String,
+    emailAddress: String,
+    linkedInProfile: String,
+    otherSocialMedia: String,
+  },
+});
+
+resumeSchema.set("toJSON", {
+  virtuals: true,
+});
+
+const Resume = mongoose.model("Resume", resumeSchema);
+
+const getAll = async () => {
+  try {
+    const resumes = await Resume.find();
+    return resumes;
+  } catch (error) {
+    return error;
+  }
+};
+
+const getOne = async (id) => {
+  try {
+    const Resume = await Resume.findById(id);
+    return Resume;
+  } catch (error) {
+    return error;
+  }
+};
+
+const create = async (body) => {
+  try {
+    const Resume = await Resume.create(body);
+    return Resume;
+  } catch (error) {
+    return error;
+  }
+};
+
+const update = async (id, body) => {
+  try {
+    const updatedResume = await Resume.findByIdAndUpdate(
+      id,
+      { ...body, updatedAt: new Date() },
+      { new: true }
+    );
+    return updatedResume;
+  } catch (error) {
+    return error;
+  }
+};
+
+const deleteResume = async (id) => {
+  try {
+    const deletedResume = await Resume.findByIdAndDelete(id);
+
+    if (!deletedResume) {
+      throw new Error("Resume item is not found");
+    }
+    return deleteResume;
+  } catch (error) {
+    return error;
+  }
+};
+
+const getByUser = async (id) => {
+  try {
+    const resumes = await Resume.find({ user: id });
+    return resumes;
+  } catch (error) {
+    return error;
+  }
+};
+
+module.exports = {
+  getAll,
+  getOne,
+  create,
+  update,
+  deleteResume,
+  Resume,
+  getByUser,
+};
