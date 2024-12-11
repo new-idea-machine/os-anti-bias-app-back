f0528c129e698c90679b990487e2c4026cc791e1
Koji Eguchi <koji.eguchi@hotmail.com>
2024-12-11 10:41:13 -0500

    Create and apply config file

---
+++ jest.setup.js
@@ -0,0 +1,32 @@
+const dotenv = require("dotenv");
+
+dotenv.config({ path: ".env.test" });
+
+// jest.setup.js
+const { MongoMemoryServer } = require("mongodb-memory-server");
+const mongoose = require("mongoose");
+
+let mongoServer;
+
+beforeAll(async () => {
+  // Start the in-memory MongoDB server
+  mongoServer = await MongoMemoryServer.create();
+
+  // Get the MongoDB URI for the in-memory database
+  const mongoUri = mongoServer.getUri();
+
+  // Set the MONGO_URI environment variable to point to the in-memory MongoDB URI
+  process.env.MONGO_URI = mongoUri;
+
+  // Connect Mongoose to the in-memory MongoDB server instead of the cloud
+  await mongoose.connect(mongoUri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+});
+
+afterAll(async () => {
+  // Disconnect mongoose and stop the in-memory MongoDB server
+  await mongoose.disconnect();
+  await mongoServer.stop();
+});
