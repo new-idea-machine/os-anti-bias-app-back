335e74b6469daa36a1ef17b90d5ffdde1e471bde
Koji Eguchi <koji.eguchi@hotmail.com>
2025-01-28 19:47:26 -0500

    Add based codes for testing employer controller

---
+++ tests/controllers/employerController.test.js
@@ -0,0 +1,6 @@
+const request = require("supertest"); //JS library for testing HTTP servers
+const server = require("../../server");
+const { v4: uuidv4 } = require("uuid");
+const { Employer } = require("../../db/models/employers");
+
+describe("Employer Controller Tests", () => {});
