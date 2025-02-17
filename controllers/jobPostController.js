a843dfdafff74da51304e5fbb7fcf2dddc975e08
Koji Eguchi <koji.eguchi@hotmail.com>
2025-02-16 19:37:04 -0500

    Refactor error handling logics to show status code

--- controllers/jobPostController.js
+++ controllers/jobPostController.js
@@ -5,35 +5,29 @@ const jwt = require("jsonwebtoken");
 const getAll = async (req, res) => {
   try {
     const jobPost = await JobPost.getAll();
-    res.send(jobPost);
+    res.status(200).send(jobPost);
   } catch (error) {
-    res.status(500).send(error);
+    res.status(error.statusCode || 500).json({ message: error.message });
   }
 };
 
 const getOne = async (req, res) => {
   try {
     const jobPost = await JobPost.getOne(req.params.id);
-    res.send(jobPost);
+    res.status(200).send(jobPost);
   } catch (error) {
-    res.status(500).send(error);
+    res.status(error.statusCode || 500).json({ message: error.message });
   }
 };
 
 const create = async (req, res) => {
   try {
     const token = req.headers["authorization"];
-
     const tokenParts = token.split(" ");
-
     const decoded = jwt.verify(tokenParts[1], process.env.JWT_SECRET);
-
     const userId = decoded.user.id;
-
     const employer = await Employer.getByUser(userId);
-
     const employerId = employer.employer_id;
-
     const newJobPost = {
       ...req.body,
       user: userId,
@@ -43,28 +37,27 @@ const create = async (req, res) => {
     };
 
     const jobPost = await JobPost.create(newJobPost);
-    res.send(jobPost);
+    res.status(201).send(jobPost);
   } catch (error) {
-    res.status(500).send(error);
+    res.status(error.statusCode || 500).json({ message: error.message });
   }
 };
 
 const update = async (req, res) => {
   try {
     const updatedJobPost = await JobPost.update(req.params.id, req.body);
-    res.send(updatedJobPost);
+    res.status(200).send(updatedJobPost);
   } catch (error) {
-    res.status(500).send(error);
+    res.status(error.statusCode || 500).json({ message: error.message });
   }
 };
 
 const deleteJobPost = async (req, res) => {
   try {
     const deletedJobPost = await JobPost.deleteJobPost(req.params.id);
-
-    res.send(deletedJobPost.id);
+    res.status(200).send(deletedJobPost.id);
   } catch (error) {
-    res.status(500).send(error);
+    res.status(error.statusCode || 500).json({ message: error.message });
   }
 };
 
@@ -72,49 +65,52 @@ const getByEmployer = async (req, res) => {
   try {
     const employerId = req.params.employerId;
     const jobPosts = await JobPost.getByEmployer(employerId);
-    res.json(jobPosts);
+    res.status(200).json(jobPosts);
   } catch (error) {
-    res.status(500).send(error);
+    res.status(error.statusCode || 500).json({ message: error.message });
   }
 };
 
 const canEdit = async (req, res) => {
   try {
     const jobPost = await JobPost.getOne(req.params.id);
-
     const token = req.headers["authorization"];
-
     const tokenParts = token.split(" ");
-
     const decoded = jwt.verify(tokenParts[1], process.env.JWT_SECRET);
-
     const userId = decoded.user.id;
-
     const canEdit = jobPost.user === userId;
-
-    return res.json({ canEdit });
+    return res.status(200).json({ canEdit });
   } catch (error) {
     console.error("Error checking edit permission:", error);
-    return res.status(500).json({ message: "Server error" });
+    res.status(error.statusCode || 500).json({ message: error.message });
+    // return res.status(500).json({ message: "Server error" });
   }
 };
 
-const getFiltered = async(req, res) => {
-  const searchString = req.query.searchString ? req.query.searchString.toLowerCase() : '';
-  const filters = req.query.filters ? JSON.parse(req.query.filters) : {};
-  const jobPosts = await JobPost.getAll();
-  const filteredJobs = jobPosts.filter(job => {
-    const matchesSearch = job.description.toLowerCase().includes(searchString) || job.job_title.toLowerCase().includes(searchString);
-    const matchesFilters = Object.keys(filters).every(key => {
-      const filterValue = filters[key];
-      return filterValue === undefined || job[key] === filterValue;
+const getFiltered = async (req, res) => {
+  try {
+    const searchString = req.query.searchString
+      ? req.query.searchString.toLowerCase()
+      : "";
+    const filters = req.query.filters ? JSON.parse(req.query.filters) : {};
+    const jobPosts = await JobPost.getAll();
+    const filteredJobs = jobPosts.filter((job) => {
+      const matchesSearch =
+        job.description.toLowerCase().includes(searchString) ||
+        job.job_title.toLowerCase().includes(searchString);
+      const matchesFilters = Object.keys(filters).every((key) => {
+        const filterValue = filters[key];
+        return filterValue === undefined || job[key] === filterValue;
+      });
+
+      return matchesSearch && matchesFilters;
     });
 
-    return matchesSearch && matchesFilters;
-  });
-
-  res.json(filteredJobs);
-}
+    res.status(200).json(filteredJobs);
+  } catch (error) {
+    res.status(error.statusCode || 500).json({ message: error.message });
+  }
+};
 
 module.exports = {
   getAll,
