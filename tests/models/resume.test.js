dd890d0205b527eb9d30c4faee2dcd4eada4f625
Koji Eguchi <koji.eguchi@hotmail.com>
2025-03-19 20:54:03 -0400

    Add a test for a method that fetch all resume

--- tests/models/resume.test.js
+++ tests/models/resume.test.js
@@ -10,85 +10,97 @@ const {
 
 const { v4: uuidv4 } = require("uuid");
 
-const mockResume = {
-  resume_id: uuidv4(),
-  user: 1,
-  title: "Frontend Developer",
-  summary:
-    "Creative frontend developer with a keen eye for design and user experience.",
-  skills: ["HTML", "CSS", "JavaScript", "React", "Vue.js"],
-  education: [
-    {
-      degree: "Bachelor of Arts in Web Design",
-      school: "Creative Institute",
-      major: "Web Design",
-      graduationYear: 2019,
-    },
-  ],
-  workExperience: [
-    {
-      jobTitle: "UI/UX Designer",
-      company: "DesignStudio",
-      location: "DesignCity",
-      startDate: "2020-02-15",
-      endDate: "2021-12-31",
-      responsibilities: [
-        "Designed and implemented user interfaces",
-        "Collaborated with development teams",
-      ],
-      achievements: [
-        "Redesigned website resulting in a 30% increase in user engagement",
-      ],
-    },
-  ],
-  projects: [
-    {
-      projectName: "Portfolio Website",
-      description:
-        "Developed a personal portfolio website showcasing design and development projects.",
-      rolesResponsibilities: ["Designed and implemented the entire website"],
-      technologiesUsed: ["React", "CSS"],
-    },
-  ],
-  certifications: [
-    {
-      certificationName: "UI/UX Design Certification",
-      issuingOrganization: "Design Academy",
-      dateEarned: "2020-06-10",
-    },
-  ],
-  languages: [
-    {
-      languageName: "JavaScript",
-      proficiencyLevel: "Advanced",
-    },
-    {
-      languageName: "CSS",
-      proficiencyLevel: "Advanced",
-    },
-    {
-      languageName: "Vue.js",
-      proficiencyLevel: "Intermediate",
-    },
-  ],
-  references: [
-    {
-      referenceName: "Emily Johnson",
-      relationship: "Former Manager",
-      contactInformation: "emily.johnson@example.com",
-    },
-  ],
-  contactInformation: {
-    phoneNumber: "+9876543210",
-    emailAddress: "john.smith@example.com",
-    linkedInProfile: "https://www.linkedin.com/in/johnsmith",
-    otherSocialMedia: "https://github.com/johnsmith",
-  },
-};
+describe("Resume Model Tests", () => {
+  let testResumeId;
 
-afterEach(async () => {
-  // Clear data between tests
-  await Resume.deleteMany({});
+  const mockResume = {
+    resume_id: uuidv4(),
+    user: 1,
+    title: "Frontend Developer",
+    summary:
+      "Creative frontend developer with a keen eye for design and user experience.",
+    skills: ["HTML", "CSS", "JavaScript", "React", "Vue.js"],
+    education: [
+      {
+        degree: "Bachelor of Arts in Web Design",
+        school: "Creative Institute",
+        major: "Web Design",
+        graduationYear: 2019,
+      },
+    ],
+    workExperience: [
+      {
+        jobTitle: "UI/UX Designer",
+        company: "DesignStudio",
+        location: "DesignCity",
+        startDate: "2020-02-15",
+        endDate: "2021-12-31",
+        responsibilities: [
+          "Designed and implemented user interfaces",
+          "Collaborated with development teams",
+        ],
+        achievements: [
+          "Redesigned website resulting in a 30% increase in user engagement",
+        ],
+      },
+    ],
+    projects: [
+      {
+        projectName: "Portfolio Website",
+        description:
+          "Developed a personal portfolio website showcasing design and development projects.",
+        rolesResponsibilities: ["Designed and implemented the entire website"],
+        technologiesUsed: ["React", "CSS"],
+      },
+    ],
+    certifications: [
+      {
+        certificationName: "UI/UX Design Certification",
+        issuingOrganization: "Design Academy",
+        dateEarned: "2020-06-10",
+      },
+    ],
+    languages: [
+      {
+        languageName: "JavaScript",
+        proficiencyLevel: "Advanced",
+      },
+      {
+        languageName: "CSS",
+        proficiencyLevel: "Advanced",
+      },
+      {
+        languageName: "Vue.js",
+        proficiencyLevel: "Intermediate",
+      },
+    ],
+    references: [
+      {
+        referenceName: "Emily Johnson",
+        relationship: "Former Manager",
+        contactInformation: "emily.johnson@example.com",
+      },
+    ],
+    contactInformation: {
+      phoneNumber: "+9876543210",
+      emailAddress: "john.smith@example.com",
+      linkedInProfile: "https://www.linkedin.com/in/johnsmith",
+      otherSocialMedia: "https://github.com/johnsmith",
+    },
+  };
+
+  afterEach(async () => {
+    // Clear data between tests
+    await Resume.deleteMany({});
+  });
+
+  describe("Create a resume", () => {
+    it("should create a resume", async () => {
+      const resume = await create(mockResume);
+      testResumeId = resume.resume_id;
+
+      expect(resume).toHaveProperty("title", mockResume.title);
+      expect(resume).toHaveProperty("resume_id", mockResume.resume_id);
+    });
+  });
 });
-
-describe("Resume Model Tests", () => {});
